82274922cc24f54ca8fef0b52c7e91b5e64709aa
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-17 19:02:27 -0500

    added connection to database to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import db from "./db/client.js";
 
 const PORT = process.send.PORT ?? 3000;
+
+await db.connect();
 
 app.listen(PORT, () => {
   console.log(`Listenting on port ${PORT}.`);
