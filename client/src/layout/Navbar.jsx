559faaa1346fc9aef72be99fbb8e8a86e3df1079
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-22 18:59:17 -0500

    Added styling to navbar as well as switched color palette for app

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -4,13 +4,24 @@ import "./navbar.css";
 export default function Navbar() {
   return (
     <div id="navbar">
-      <div>Star Gazing</div>
-      <div className="navLinks">
-        <NavLink>Posts</NavLink>
-        <NavLink>Events</NavLink>
-        <NavLink>Schedule</NavLink>
+      <div className="pageName">
+        <NavLink to="/">Star Gazing</NavLink>
       </div>
-      <div>Login</div>
+      <div className="navLinks">
+        <NavLink to="/posts" className={({ isActive }) => (isActive ? "activeSite" : "")}>
+          Posts
+        </NavLink>
+        <NavLink to="/events" className={({ isActive }) => (isActive ? "activeSite" : "")}>
+          Events
+        </NavLink>
+        <NavLink to="/schedule" className={({ isActive }) => (isActive ? "activeSite" : "")}>
+          Schedule
+        </NavLink>
+        <div className="accountContainer">
+          <NavLink>Login / Sign Up</NavLink>
+          {/* change to switch to account button when logged in */}
+        </div>
+      </div>
     </div>
   );
 }
