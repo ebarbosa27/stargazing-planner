c296c0c0a44426dbd7f937f3f5173bab319d1cf4
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-22 09:27:26 -0500

    added createUser query that will add user data to db and seed some user data

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,8 +1,24 @@
 import db from "./client.js";
+import { faker } from "@faker-js/faker";
+import { createUser } from "./queries/users.js";
 
 await db.connect();
 await seed();
 await db.end();
 console.log("Database seeded.");
 
-async function seed() {}
+async function seed() {
+  // create user
+  // create event created by user
+  // create hotspot created by user
+  // create rsvp for event and is linked to user that is rsvp'ing
+  // create favorite for event created by user
+
+  for (let i = 0; i < 5; i++) {
+    const userData = {
+      username: faker.internet.username(),
+      password: faker.internet.password(),
+    };
+    const result = await createUser(userData);
+  }
+}
