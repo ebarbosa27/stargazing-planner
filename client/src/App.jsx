976dc44b16238d8effc16ce160b89ead54fc16d8
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-22 14:06:50 -0500

    started with a basic route product with a homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 
+import Home from "./pages/Home";
+
 function App() {
-  return <></>;
+  return (
+    <div id="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
