aaef00a6fdb7b607020405c6790eab6819dc23c1
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-22 19:17:44 -0500

    small formatting

--- server/db/queries/events.js
+++ server/db/queries/events.js
@@ -2,11 +2,11 @@ import db from "../client.js";
 
 export async function createEvent({ date, location, userId }) {
   const sql = `
-INSERT INTO events 
-  (date, user_id, location)
-VALUES
-  ($1, $2, ST_SetSRID(ST_MakePoint($3, $4), 4326))
-RETURNING *
+  INSERT INTO events 
+    (date, user_id, location)
+  VALUES
+    ($1, $2, ST_SetSRID(ST_MakePoint($3, $4), 4326))
+  RETURNING *
 `;
   const {
     rows: [event],
