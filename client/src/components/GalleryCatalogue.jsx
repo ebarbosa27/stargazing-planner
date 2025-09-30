99328d7438366d141ef6b4fa2e2dafc17d0e62d4
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-30 17:20:36 -0500

    fixed issue that caused scroll on gallery to affect window

--- client/src/components/GalleryCatalogue.jsx
+++ client/src/components/GalleryCatalogue.jsx
@@ -1,24 +1,33 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function GalleryCatalogue({ data }) {
   const galleryRef = useRef();
 
-  function handleGalleryScroll(e) {
-    e.preventDefault();
-
+  useEffect(() => {
     const el = galleryRef.current;
-    if (e.deltaY === 0) return;
+    const handleGalleryScroll = (e) => {
+      e.preventDefault();
+      if (e.deltaY === 0) return;
 
-    let scrollDirection = 1;
-    if (e.deltaY < 0) scrollDirection = -1;
+      const el = galleryRef.current;
+      let scrollDirection = 1;
+      if (e.deltaY < 0) scrollDirection = -1;
 
-    el.scrollTo({
-      left: el.scrollLeft + e.target.width * scrollDirection,
-      behavior: "smooth",
-    });
-  }
+      el.scrollTo({
+        left: el.scrollLeft + e.target.width * scrollDirection,
+        behavior: "smooth",
+      });
+    };
+
+    el.addEventListener("wheel", handleGalleryScroll, { passive: false });
+
+    return () => {
+      el.removeEventListener("wheel", handleGalleryScroll);
+    };
+  }, []);
+
   return (
-    <ul id="eventGallery" onWheel={handleGalleryScroll} ref={galleryRef}>
+    <ul id="eventGallery" ref={galleryRef}>
       {data.image_urls.map((eventImage, idx) => {
         return (
           <li key={data.event + "_" + idx}>
