da37c4a676b3892dfc9cad211ac2df16e1fe96a3
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-30 19:52:06 -0500

    account page event catalogue now has horizontal scroll feature

--- client/src/components/EventCatalogue.jsx
+++ client/src/components/EventCatalogue.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useRef } from "react";
+
 export default function EventCatalogue({ events, componentName }) {
+  const catalogueRef = useRef();
+
+  useEffect(() => {
+    const el = catalogueRef.current;
+    const handleWheelScroll = (e) => {
+      e.preventDefault();
+      if (e.deltaY === 0) return;
+
+      let scrollDirection = 1;
+      if (e.deltaY < 0) scrollDirection = -1;
+
+      el.scrollTo({
+        left: el.scrollLeft + e.target.width * scrollDirection,
+        behavior: "smooth",
+      });
+    };
+
+    el.addEventListener("wheel", handleWheelScroll, { passive: false });
+
+    return () => {
+      el.removeEventListener("wheel", handleWheelScroll);
+    };
+  }, []);
+
   return (
-    <ul className="catalogueRow">
+    <ul className="catalogueRow" ref={catalogueRef}>
       {events
         ? events.map((event) => {
             return (
