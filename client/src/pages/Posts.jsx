0277a21f035bcf4eefecaed5ea3c73ec7ccb7416
ebarbosa27 <ebarbosa5449@gmail.com>
2025-10-04 14:50:36 -0500

    added post page that will get events data from open source api and include it in posts

--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -1,7 +1,39 @@
+import useQuery from "../api/useQuery";
+
 export default function Posts() {
+  const { data, loading, error } = useQuery("/news", "news");
+
+  if (loading) return <div>Loading. . . </div>;
+  if (error) return <div>Error Occured </div>;
+
   return (
-    <>
-      <h2>Posts Page</h2>
-    </>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h2>News Page</h2>
+      <ol>
+        {data?.results
+          ? data?.results.map((newsEvent) => {
+              console.log(newsEvent);
+              return (
+                <li>
+                  <div style={{ width: "300px", aspectRatio: "1" }}>
+                    <img
+                      src={newsEvent.image_url}
+                      alt=""
+                      style={{ width: "300px", aspectRatio: "1" }}
+                    />
+                  </div>
+                  <h3>{newsEvent.title}</h3>
+                  <p>{newsEvent.summary}</p>
+                </li>
+              );
+            })
+          : ""}
+      </ol>
+    </div>
   );
 }
--- server/api/news.js
+++ server/api/news.js
@@ -2,11 +2,15 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-const API_URL = "https://api.astronomyapi.com";
+const API_URL = "https://api.spaceflightnewsapi.net";
 
 router.route("/").get(async (req, res) => {
   try {
-    res.status(200).json({ messege: "complete" });
+    let fetchUrl = API_URL + "/v4/articles/?limit=10";
+    const response = await fetch(fetchUrl);
+    const responseJson = await response.json();
+
+    res.status(200).json({ results: responseJson.results });
   } catch (err) {
     console.log("Error occurred");
     console.log(err);
