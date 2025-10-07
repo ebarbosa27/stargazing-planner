b04f3cf6e5e3e0a23fe7695f89d5b80534535876
ebarbosa27 <ebarbosa5449@gmail.com>
2025-10-06 21:03:56 -0500

    updated api url to be a env var

--- client/src/api/ApiContext.jsx
+++ client/src/api/ApiContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
 import { useAuth } from "../auth/AuthContext";
 
-export const API = "http://localhost:3000";
+export const API = import.meta.env.VITE_API_URL;
 const ApiContext = createContext();
 
 export function ApiProvider({ children }) {
